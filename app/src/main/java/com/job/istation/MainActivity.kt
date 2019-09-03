dac7c0f7b9ecb7dfbad6338a9d207d1a09675482
JobGetabu <getabujob@gmail.com>
2019-09-03 15:31:22 +0300

    designed home dashboard

--- app/src/main/java/com/job/istation/MainActivity.kt
+++ app/src/main/java/com/job/istation/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.job.istation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+
+        setSupportActionBar(toolbar)
+        Tools.setSystemBarColor(this, R.color.grey_5)
+        Tools.setSystemBarLight(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        Tools.changeMenuIconColor(menu, resources.getColor(R.color.grey_60))
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.getItemId() === android.R.id.home) {
+            finish()
+        } else {
+            Toast.makeText(applicationContext, item.title, Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
